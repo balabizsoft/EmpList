1030124b2c0de3859a8cae7fd658fa5e81701693
bala <bala.bizsoft@gmail.com>
2019-07-13 10:37:33 +0800

    Add project files.

---
+++ WebApplication43/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace WebApplication43.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public string EmpList1()
+        {
+            string result = "";
+            
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "/MyData/EmpList.txt";
+            StreamReader streamReader = new StreamReader(filePath);
+            string Rec = "";
+
+            Rec = streamReader.ReadLine();
+            while (Rec != null)
+            {
+                result += Rec;
+                Rec = streamReader.ReadLine();
+            }
+
+            return result;
+        }
+
+        public ActionResult EmpList2()
+        {
+            List<Models.Employee> result = new List<Models.Employee>();
+
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "/MyData/EmpList.txt";
+            StreamReader streamReader = new StreamReader(filePath);
+            string Rec = "";
+
+            Rec = streamReader.ReadLine();
+            while (Rec != null)
+            {
+                result.Add(new Models.Employee(Rec));
+                Rec = streamReader.ReadLine();
+            }
+
+
+            return View(result);
+        }
+
+        public ActionResult EmpList3(string orderBy)
+        {
+            List<Models.Employee> result = new List<Models.Employee>();
+
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "/MyData/EmpList.txt";
+            StreamReader streamReader = new StreamReader(filePath);
+            string Rec = "";
+
+            Rec = streamReader.ReadLine();
+            while (Rec != null)
+            {
+                result.Add(new Models.Employee(Rec));
+                Rec = streamReader.ReadLine();
+            }
+
+            switch (orderBy)
+            {
+                case "nameasc":
+                    return View(result.OrderBy(x=> x.Name).ToList());
+                case "namedesc":
+                    return View(result.OrderByDescending(x => x.Name).ToList());
+                case "emailasc":
+                    return View(result.OrderBy(x => x.EMail).ToList());
+                case "emaildesc":
+                    return View(result.OrderByDescending(x => x.EMail).ToList());
+                default:
+                    return View(result);
+            }
+
+
+
+        }
+    }
+}
---
+++ WebApplication43/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication43.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EMail { get; set; }
+
+        public Employee(string record)
+        {
+            var datas = record.Split('\t');
+            Id = Convert.ToInt32(datas[0]);
+            Name = datas[1];
+            EMail = datas[2];
+        }
+    }
+}
